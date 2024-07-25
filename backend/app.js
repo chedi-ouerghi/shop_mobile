efb4a259e2529f0c4843f5e870c6866de756bdf6
chedi-ouerghi <chediouerghi88@gmail.com>
2024-07-25 14:18:19 +0100

    mise a jour du render

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config(); 
+require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
 const alcoolProductRouter = require('./routes/alcoolProductRouter');
@@ -12,9 +12,9 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(process.env.AUTH_PATH, authRoutes);
-app.use(process.env.ALCOOL_PATH, alcoolProductRouter);
-app.use(process.env.ACHAT_PATH, achatRoutes);
+app.use('/auth', authRoutes);
+app.use('/alcool', alcoolProductRouter);
+app.use('/achat', achatRoutes);
 
 const PORT = process.env.PORT || 3000; 
 app.listen(PORT, '0.0.0.0', () => {
